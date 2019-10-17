997bbd83d8dd67d2a89beb003a6ccccf977fd9bd
peipei68 <peiyun_hng@yahoo.com>
2019-10-16 21:12:42 -0500

    Homework Mission to Mars

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
-from flask import Flask, render_template
-from flask_pymongo import pymongo
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import ScrapeMars
 
 app = Flask(__name__)
 
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
-db = client.team_db
-collection = db.produce
-
-@app.route('/')
-
+@app.route("/")
 def index():
-
-    mars_omnibus= mongo.db.collection.find()
-    
-    return render_template("index.html, mars_omnibus= mars_omnibus")
-    
+   mars_omnibus = mongo.db.mars_omnibus.find_one()
+   return render_template("index.html", mars_omnibus=mars_omnibus)
 
 @app.route("/scrape")
-def scraper():
-    
-    mars_omnibus = mongo.db.collection
-    mars_data = ScrapeMars.ScrapeMarsNews()
-    mars_data = ScrapeMars.ScrapeMarsImage()
-    mars_data = ScrapeMars.ScrapeMarsWeather()
-    mars_data = ScrapeMars.ScrapeMarsFacts()
-    mars_data = ScrapeMars.ScrapeMarsHemispheres()
+def Scrape():
+   mars_omnibus = mongo.db.mars_omnibus
+   mars_data = ScrapeMars.Scrape()
+   mars_omnibus.update({}, mars_data, upsert=True)
+   return redirect("/", code=302)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(debug=True)
